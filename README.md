c0141c69a0656cbeaa85b64780088102100ffbdb
ksh030506 <llmm030506@naver.com>
2022-07-20 10:38:08 +0900

    add redis

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # template-app-of-apps-helm
+
+전부 aaa 아니면 aaaa로 치환하였습니다.
+검색해서 바꿔주세요!!!!
--- redis/install-or-update.sh
+++ redis/install-or-update.sh
@@ -1,5 +1,3 @@
 #! /bin/bash
 
-## with context dano-kube
-#helm upgrade dano-redis bitnami/redis --wait --install -f values-production.yaml -n cache
 helm upgrade redis bitnami/redis --wait --install -f values-production.yaml -n cache
